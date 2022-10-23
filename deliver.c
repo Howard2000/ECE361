3949837cd908761158aaf9c3d772a5f272e8f15c
Hao Zhu <howard.zhu@mail.utoronto.ca>
2022-10-23 02:01:43 -0400

    lab1 section2

--- deliver.c
+++ deliver.c
@@ -10,10 +10,86 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define MAX_FILE_SIZE 1000*5 //5 MB (no use)
+
+struct packet{
+	unsigned int total_frag;
+
+	unsigned int frag_no;
+	unsigned int size;
+	char * filename;
+	char filedata[1000]; //1 KB
+};
+
+void intToString(int n, char * s_num);
+
+void progress_bar(double percentage){
+	int x;
+	printf("[");
+	for(x=0;x<=percentage*30;x++){
+		printf(">");
+	}
+	for(x=percentage*30;x<30;x++){
+		printf("-");
+	}
+	printf("] %6.2lf%% completed\r",(double)(percentage*100));
+	fflush(stdout);
+	return;
+}
+
+void packUpFile(FILE * _fp, char * file_name, struct packet * pack, int pack_num, int total_pack){
+
+	size_t rd;
+	rd = fread(pack->filedata,1,1000,_fp);
+	if(rd < 0 ){
+		perror("fread");
+		exit(1);
+	}
+	// printf("data: %s",pack->filedata);
+	pack->total_frag = total_pack;
+	pack->frag_no = pack_num;
+	pack->size = rd;
+	pack->filename = file_name;
+
+	return;
+}
+
+void packToMsg(struct packet * pack, char * msg){
+	char temp[2000]={0};
+	memset(msg, 0, 2000);
+	int ptr_shift=0;
+	intToString(pack->total_frag,temp);
+	strcpy(msg,temp);
+	strcat(msg,":");
+	memset(temp,0,sizeof(temp));
+	intToString(pack->frag_no,temp);
+	strcat(msg,temp);
+	strcat(msg,":");
+	memset(temp,0,sizeof(temp));
+	intToString(pack->size,temp);
+	strcat(msg,temp);
+	strcat(msg,":");
+	strcat(msg,pack->filename);
+	strcat(msg,":");
+	ptr_shift = strlen(msg);
+	for(int i = 0;i<pack->size;i++){
+		msg[i+ptr_shift]=pack->filedata[i];
+	}
+	return;
+}
+
 
 void usage(){
 	printf("Usage: deliver <server address> <server port number>\n");
 	exit(1);
+}
+
+void intToString(int n, char * s_num){
+	sprintf(s_num,"%d",n);
+	return;
 }
 
 int transferToInt(char s[]){
@@ -33,16 +109,23 @@ int transferToInt(char s[]){
 int main(int argc, char * argv[]){
 	printf("Starting client...\n");
 
-	int socket_fd, server_port, fd;
+	//declear variables
+	int socket_fd, server_port,  pack_ptr=0, total=0;
 	struct sockaddr_in server_addr;
-	char server_msg[1000], client_msg[1000], file_path[1000];
+	char server_msg[2000], client_msg[2000], file_path[2000];
 	in_addr_t nbo_addr;
 	socklen_t server_addr_length = sizeof(server_addr);
-
+	struct timeval startT,endT;
+	struct packet pack;
+	double timeuse;
+	int next = 1;
+	FILE * fp;
+	
+	//initailization 
 	memset(server_msg, '\0', sizeof(server_msg));
     memset(client_msg, '\0', sizeof(client_msg));
 	memset(file_path, '\0', sizeof(file_path));
-
+	
 	if(argc != 3){
 		usage();
 	}
@@ -50,7 +133,7 @@ int main(int argc, char * argv[]){
 		nbo_addr = inet_addr(argv[1]);
 		// printf("%x\n", nbo_addr);
 		if(nbo_addr < 0){
-			perror("Invalid address\n");
+			perror("inet_addr");
 			exit(1);
 		}
 
@@ -60,22 +143,27 @@ int main(int argc, char * argv[]){
 		}
 	}
 
+	//crate socket
 	socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if(socket_fd<0){
-		perror("error");
+		perror("socket");
 		exit(1);
 	}
-
+	//set socket parameters
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(server_port);
 	server_addr.sin_addr.s_addr = nbo_addr;
 	printf("ftp command: ftp <file name>\n");
 	printf(">>");
-	scanf("%s %s", client_msg, file_path);
 
+	//input
+	scanf("%s %s", client_msg, file_path);
+	//input vaildating
 	if(strcmp("ftp",client_msg) == 0){
-		if((fd = open(file_path, O_RDONLY)) < 0){
-			perror("error");
+		//opoen file
+		fp = fopen(file_path,"rb");
+		if(fp==NULL){
+			perror("fopen");
 			exit(1);
 		}
 	}
@@ -83,27 +171,80 @@ int main(int argc, char * argv[]){
 		printf("command not found\n");
 		exit(1);
 	}
-	
+
+	//send msg
+	//get start time
+	gettimeofday(&startT,NULL);
+	//get expected packets number
+	fseek(fp,0L,SEEK_END);
+	pack_ptr=0;
+	total = ftell(fp)/1000 + 1; 
+	//reset file stream pointer
+	fseek(fp,0L,SEEK_SET);
+	// printf("%d\n",total);
 	if(sendto(socket_fd, client_msg, sizeof(client_msg), 0, 
 		(struct sockaddr*)&server_addr, server_addr_length) < 0){
-		perror("error: sending message failed\n");
+		perror("sendto");
 		exit(1);
 	}
 
+	//receive
 	if(recvfrom(socket_fd, server_msg, sizeof(server_msg), 0, 
 		(struct sockaddr*)&server_addr, &server_addr_length) < 0){
-		perror("error: receiving message failed\n");
+		perror("recvfrom");
 		exit(1);
 	}
 
-	printf("%s\n", server_msg);
+	//output result
+	// printf("%s\n", server_msg);
+	// printf("%d %d\n",pack_ptr, total);
 	if(strcmp("yes",server_msg) == 0){
 		printf("A file transfer can start.\n");
+		while(pack_ptr < total){
+			// printf("%d %d\n",pack_ptr, total);
+			progress_bar((double)(pack_ptr/(double)total));
+			if(next){
+				packUpFile(fp,file_path,&pack,pack_ptr,total);
+			}
+			// printf("%d\n",pack.total_frag);
+			// printf("%d\n",pack.frag_no);
+			// printf("%d\n",pack.size);
+			// printf("%s\n",pack.filename);
+			packToMsg(&pack,client_msg);
+			// printf("\n%s\n",client_msg);
+			if(sendto(socket_fd, client_msg, sizeof(client_msg), 0, 
+				(struct sockaddr*)&server_addr, server_addr_length) < 0){
+				perror("\nsendto");
+				exit(1);
+			}
+			if(recvfrom(socket_fd, server_msg, sizeof(server_msg), 0, 
+				(struct sockaddr*)&server_addr, &server_addr_length) < 0){
+				perror("\nrecvfrom");
+				exit(1);
+			}
+			// printf("%s\n",server_msg);
+			if(strcmp("ACK",server_msg) == 0){
+				pack_ptr++;
+				next = 1;
+			}
+			else{
+				next = 0;
+			}
+
+		}
+		progress_bar((double)(pack_ptr/(double)total));
+		
 	}
 	else{
 		exit(1);
 	}
-	close(fd);
+	gettimeofday(&endT,NULL);
+	//calcuate time lapses
+	timeuse = (endT.tv_sec-startT.tv_sec) + (double)(endT.tv_usec-startT.tv_usec)/CLOCKS_PER_SEC;
+	printf("\n");
+	printf("Successfully transfered, time lapses: %lf seconds\n",timeuse);
+	//close file and socket
+	fclose(fp);
 	close(socket_fd);
 	return 0;
 }
--- server.c
+++ server.c
@@ -8,6 +8,118 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <fcntl.h>
+
+
+#define MAX_FILE_SIZE 100000 //100 MB (no use)
+
+struct packet{
+	unsigned int total_frag;
+
+	unsigned int frag_no;
+	unsigned int size;
+	char * filename;
+	char filedata[1000]; //1 KB
+};
+
+int transferToInt(char s[]);
+
+void unpack(struct packet * pack, char * msg){
+	int ptr=0,col_num=0,strl;
+	char temp[2000]={0};
+	strl=strlen(msg);
+	for(int i=0;i<strl;i++){
+		// printf("i: %d\n",i);
+		if(col_num>3){
+			// printf("%d\n",i-ptr);
+			for(int j=0;j<pack->size;j++){
+				pack->filedata[j]=msg[j+ptr];
+			}
+			break;
+		}
+		else if(msg[i]==':'){
+			// printf("ptr: %d\n",ptr);
+			switch (col_num){
+				case 0:
+					memset(temp,0,sizeof(temp));
+					strncpy(temp,msg+ptr,i-ptr);
+					// for(int j=0;j<i-ptr;j++){
+					// 	temp[j]=msg[j+ptr];
+					// }
+					// temp[i-ptr]='\0';
+					pack->total_frag = transferToInt(temp);
+					ptr=i+1;
+					
+					break;
+				case 1:
+					memset(temp,0,sizeof(temp));
+					strncpy(temp,msg+ptr,i-ptr);
+					// for(int j=0;j<i-ptr;j++){
+					// 	temp[j]=msg[j+ptr];
+					// }
+					// temp[i-ptr]='\0';
+					pack->frag_no = transferToInt(temp);
+					ptr=i+1;
+					
+					break;
+				case 2:
+					memset(temp,0,sizeof(temp));
+					strncpy(temp,msg+ptr,i-ptr);
+					// for(int j=0;j<i-ptr;j++){
+					// 	temp[j]=msg[j+ptr];
+					// }
+					// temp[i-ptr]='\0';
+					pack->size = transferToInt(temp);
+					ptr=i+1;
+					
+					break;
+				case 3:
+					memset(temp,0,sizeof(temp));
+					strncpy(temp,msg+ptr,i-ptr);
+					// for(int j=0;j<i-ptr;j++){
+					// 	temp[j]=msg[j+ptr];
+					// }
+					// temp[i-ptr]='\0';
+					pack->filename = temp;
+					ptr=i+1;
+					
+					break;
+				default:
+					break;
+			}
+			col_num++;
+			
+		}
+		
+	}
+	return;
+}
+
+void progress(double perc){
+	int x;
+	x = (int)perc%4;
+	switch(x){
+		case 0:
+			printf(" %6.2lf%% -\r",perc);
+			fflush(stdout);
+			break;
+		case 1:
+			printf(" %6.2lf%% \\\r",perc);
+			fflush(stdout);
+			break;
+		case 2:
+			printf(" %6.2lf%% |\r",perc);
+			fflush(stdout);
+			break;
+		case 3:
+			printf(" %6.2lf%% /\r",perc);
+			fflush(stdout);
+			break;	
+		default:
+			break;
+	}
+	return;
+}
 
 void usage(){
 	printf("Usage: server <UDP listen port>\n");
@@ -30,14 +142,21 @@ int transferToInt(char s[]){
 
 int main(int argc, char * argv[]){
 	printf("Starting server...\n");
-	int socket_fd = 0,port_num = 0;
-	struct sockaddr_in server_addr, client_addr;
-	char client_msg[1000],server_msg[1000];
-	socklen_t client_addr_length = sizeof(client_addr);
 
+	//declear vailables
+	int socket_fd = 0,port_num = 0,ack_pk=0,total_pk;
+	int set=0, opened=0;
+	struct sockaddr_in server_addr, client_addr;
+	char client_msg[2000],server_msg[2000];
+	socklen_t client_addr_length = sizeof(client_addr);
+	struct packet pack;
+	FILE * fp;
+	int ret;
+	
+	//initializion
 	memset(server_msg, '\0', sizeof(server_msg));
     memset(client_msg, '\0', sizeof(client_msg));
-
+	//input validation
 	if(argc != 2){
 		usage();
 	}
@@ -47,10 +166,10 @@ int main(int argc, char * argv[]){
 			usage();
 		}
 	}
-
+	//setup socket
 	socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if(socket_fd<0){
-		perror("error: socket started failed\n");
+		perror("socket");
 		exit(1);
 	}
 
@@ -58,16 +177,17 @@ int main(int argc, char * argv[]){
     server_addr.sin_port = htons(port_num);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
-
+	//bind socket
 	if(bind(socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
-		perror("error: binding failed\n");
+		perror("bind");
 		exit(1);
 	}
 
+	//start listing
 	if(recvfrom(socket_fd, client_msg, sizeof(client_msg), 0, 
 		(struct sockaddr*)&client_addr, &client_addr_length) < 0){
 
-		perror("error: receiving failed\n");
+		perror("recvfrom");
 		exit(1);
 	}
 	printf("Received message from %s: %s\n",
@@ -83,9 +203,95 @@ int main(int argc, char * argv[]){
 	if (sendto(socket_fd, server_msg, sizeof(server_msg), 0, 
 		(struct sockaddr*)&client_addr, client_addr_length) < 0){
 
-        printf("error: sending failed\n");
+        printf("sendto");
         exit(1);
     }
+	//file transfer sesson
+	do{
+
+		if(recvfrom(socket_fd, client_msg, sizeof(client_msg), 0, 
+			(struct sockaddr*)&client_addr, &client_addr_length) < 0){
+
+			perror("\nrecvfrom");
+			exit(1);
+		}
+		// printf("\n%s\n",client_msg);
+		unpack(&pack,client_msg);
+		// printf("%d\n",pack.total_frag);
+		// printf("%d\n",pack.frag_no);
+		// printf("%d\n",pack.size);
+		// printf("%s\n",pack.filename);
+		// printf("%s\n",pack.filedata);
+		if(!set){
+			total_pk=pack.total_frag;
+			ack_pk = pack.frag_no;
+			set=1;
+		}
+		
+		// printf("%d %d\n",pack.frag_no, ack_pk);
+
+		if(pack.frag_no==ack_pk){
+			
+			// printf("receiving file: %s",pack.filename);
+			// progress((double)ack_pk/(double)total_pk*100);
+			// printf("%d\n",pack.frag_no);
+			if(!opened){
+			// printf("receiving file: %s",pack.filename);
+				fp = fopen(pack.filename,"wb");
+				if(fp==NULL){
+					perror("\nfopen");
+					exit(1);
+				}
+				else{
+					// printf("file opened successfully\n");
+				}
+				opened=1;
+			}
+			if(opened){
+				ret = fwrite(pack.filedata,1,pack.size,fp);
+				if(ret < pack.size){
+            		perror("\nfwrite");
+					exit(1);
+        		}
+				ack_pk = pack.frag_no+1;
+				strcpy(server_msg, "ACK");
+				if (sendto(socket_fd, server_msg, sizeof(server_msg), 0, 
+				(struct sockaddr*)&client_addr, client_addr_length) < 0){
+
+        			printf("\nsendto");
+        			exit(1);
+    			}
+			}
+			else{
+				strcpy(server_msg, "NACK");
+			// ack_pk = pack.frag_no;
+			// printf("%s\n",server_msg);
+				if (sendto(socket_fd, server_msg, sizeof(server_msg), 0, 
+				(struct sockaddr*)&client_addr, client_addr_length) < 0){
+
+        			printf("\nsendto");
+        			exit(1);
+    			}
+			}
+			
+		}
+		else{
+			strcpy(server_msg, "NACK");
+			// ack_pk = pack.frag_no;
+			// printf("%s\n",server_msg);
+			if (sendto(socket_fd, server_msg, sizeof(server_msg), 0, 
+			(struct sockaddr*)&client_addr, client_addr_length) < 0){
+
+        		printf("\nsendto");
+        		exit(1);
+    		}
+		}
+		
+	}while(ack_pk<total_pk);
+	// printf("receiving file: %s",pack.filename);
+	// progress((double)ack_pk/(double)total_pk*100);
+	// printf("\n");
+	fclose(fp);
 	close(socket_fd);
 	return 0;
 }
